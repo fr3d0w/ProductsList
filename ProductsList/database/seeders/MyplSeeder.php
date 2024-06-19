42021046643dd1bd9055c34e4686e7b66900107d
fr3d0w <fardini.frederic@gmail.com>
2024-06-19 10:09:11 -0400

    commitPL

---
+++ ProductsList/database/seeders/MyplSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MyplSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
