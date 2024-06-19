093b69e2ac47502b384bd50a0ee9bf7c2c1ff825
fr3d0w <fardini.frederic@gmail.com>
2024-06-19 18:39:11 -0400

    routes

---
+++ ProductsList/resources/views/pl/home.blade.php
@@ -0,0 +1,15 @@
+@extends('pl.layouts.app')
+
+@section('main')
+
+<h1>ou ja sav'</h1>
+
+@endsection
+
+
+@section('footer')
+<footer class="py-16 text-center text-sm text-black dark:text-white/70">
+    Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+</footer>
+
+@endsection
